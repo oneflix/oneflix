94e50d147fdb41d84487e4e294bb0b059029071c
yunjae <wyj10076@naver.com>
2020-02-16 15:37:15 +0900

    manager 검색 기능 테스트

--- src/main/java/com/main/oneflix/manager/admin/controller/ManagerController.java
+++ src/main/java/com/main/oneflix/manager/admin/controller/ManagerController.java
@@ -32,8 +32,9 @@ public class ManagerController {
 	@Autowired
 	private GetManagerListService getManagerListService;
 	
-	@RequestMapping("/managerList.mdo")
+	@RequestMapping("/managerListProc.mdo")
 	public ModelAndView getManagerList(ManagerVO vo, ModelAndView mav) {
+//		if (vo.getSearchManager() == null) vo.setSearchManager("");
 		List<ManagerVO> managerList = getManagerListService.getManagerList(vo);
 		mav.addObject("managerList", managerList);
 		mav.setViewName("managerList");
@@ -49,7 +50,7 @@ public class ManagerController {
 	@RequestMapping("/insertManagerProc.mdo")
 	public ModelAndView insertManagerProc(ManagerVO vo, ModelAndView mav) {
 		insertManagerService.insertManager(vo);
-		mav.setViewName("redirect:/managerList.mdo");
+		mav.setViewName("redirect:/managerListProc.mdo");
 		return mav;
 	}
 	
@@ -64,14 +65,14 @@ public class ManagerController {
 	@RequestMapping("/updateManagerProc.mdo")
 	public ModelAndView updateManagerProc(ManagerVO vo, ModelAndView mav) {
 		updateManagerSerivce.updateManager(vo);
-		mav.setViewName("redirect:/managerList.mdo");
+		mav.setViewName("redirect:/managerListProc.mdo");
 		return mav;
 	}
 	
 	@RequestMapping("/deleteManagerProc.mdo")
 	public ModelAndView deleteManagerProc(ManagerVO vo, ModelAndView mav) {
 		deleteManagerSerivce.deleteManager(vo);
-		mav.setViewName("redirect:/managerList.mdo");
+		mav.setViewName("redirect:/managerListProc.mdo");
 		return mav;
 	}
 	
