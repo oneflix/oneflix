40e40c8b7f9cbcf316d6970960b833c235f24f92
proleesh <57159010+proleesh@users.noreply.github.com>
2020-02-17 22:52:31 +0900

    고객센터 CRUD 완성(#18)
    * Admin페이지 Actor검색기능추가
    
    * FAQ 완성
    
    * db.properties삭제

---
+++ src/main/java/com/main/oneflix/help/service/impl/UpdateHelpServiceImpl.java
@@ -0,0 +1,21 @@
+package com.main.oneflix.help.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.main.oneflix.help.dao.HelpDAO;
+import com.main.oneflix.help.service.UpdateHelpService;
+import com.main.oneflix.help.vo.HelpVO;
+@Service
+public class UpdateHelpServiceImpl implements UpdateHelpService {
+
+	@Autowired
+	private HelpDAO helpDAO;
+	
+	@Override
+	public void updateHelpService(HelpVO vo) {
+		helpDAO.updateHelp(vo);
+
+	}
+
+}
