1a31d0fe0fa2880618e585a4a28b269796d94902
wyj10076 <59532709+wyj10076@users.noreply.github.com>
2020-02-19 22:11:18 +0900

    memberCRUD 기능 구현 중, 중간 push (#26)
    * 비밀번호 확인
    
    * client dao service track
    
    * 이용권관리 crud
    
    * 이용권관리 crud 작업중
    
    * Ticket CRUD 수정 및 member CRUD 수정
    
    * 회원가입유효성검사 Test
    
    * 회원가입 완료/정보수정testing/이용권관리완료
    
    Co-authored-by: jaenyes <60684629+jaenyes@users.noreply.github.com>

---
+++ src/main/java/com/main/oneflix/member/service/GetMemberService.java
@@ -0,0 +1,7 @@
+package com.main.oneflix.member.service;
+
+import com.main.oneflix.member.vo.MemberVO;
+
+public interface GetMemberService {
+	public MemberVO getMember(MemberVO vo);
+}
---
+++ src/main/java/com/main/oneflix/ticket/service/InsertTicketService.java
@@ -0,0 +1,8 @@
+package com.main.oneflix.ticket.service;
+
+import com.main.oneflix.ticket.vo.TicketVO;
+
+public interface InsertTicketService {
+	public void insertTicket(TicketVO Vo);
+
+}
