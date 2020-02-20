f57f4e9da0a775bb6f10d430546c2395057c4fd2
wyj10076 <59532709+wyj10076@users.noreply.github.com>
2020-02-20 22:19:36 +0900

    Help CRUD 작업 중간 push (#36)
    * 중간 완성버전 저장
    
    * help CRUD 작업 중간 commit

--- src/main/java/com/main/oneflix/help/service/InsertHelpService.java
+++ src/main/java/com/main/oneflix/help/service/InsertHelpService.java
@@ -3,7 +3,7 @@ package com.main.oneflix.help.service;
 import com.main.oneflix.help.vo.HelpVO;
 
 public interface InsertHelpService {
-	public void insertHelpService(HelpVO vo);
+	public void insertHelp(HelpVO vo);
 	
 
 }
