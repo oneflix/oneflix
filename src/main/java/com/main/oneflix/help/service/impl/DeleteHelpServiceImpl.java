bd6c8ea5dd52b9f861009b218978e076e9e95d95
yunjae <wyj10076@naver.com>
2020-02-20 22:18:07 +0900

    help CRUD 작업 중간 commit

--- src/main/java/com/main/oneflix/help/service/impl/DeleteHelpServiceImpl.java
+++ src/main/java/com/main/oneflix/help/service/impl/DeleteHelpServiceImpl.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Service;
 import com.main.oneflix.help.dao.HelpDAO;
 import com.main.oneflix.help.service.DeleteHelpService;
 import com.main.oneflix.help.vo.HelpVO;
+
 @Service
 public class DeleteHelpServiceImpl implements DeleteHelpService {
-	
+
 	@Autowired
 	private HelpDAO helpDAO;
 
 	@Override
-	public void deleteHelpService(HelpVO vo) {
-		// TODO Auto-generated method stub
+	public void deleteHelp(HelpVO vo) {
 		helpDAO.deleteHelp(vo);
 	}
 
--- src/main/java/com/main/oneflix/help/service/impl/GetHelpServiceImpl.java
+++ src/main/java/com/main/oneflix/help/service/impl/GetHelpServiceImpl.java
@@ -6,15 +6,15 @@ import org.springframework.stereotype.Service;
 import com.main.oneflix.help.dao.HelpDAO;
 import com.main.oneflix.help.service.GetHelpService;
 import com.main.oneflix.help.vo.HelpVO;
+
 @Service
 public class GetHelpServiceImpl implements GetHelpService {
 
 	@Autowired
 	private HelpDAO helpDAO;
-	
+
 	@Override
-	public HelpVO getHelpService(HelpVO vo) {
-		// TODO Auto-generated method stub
+	public HelpVO getHelp(HelpVO vo) {
 		return helpDAO.getHelp(vo);
 	}
 
