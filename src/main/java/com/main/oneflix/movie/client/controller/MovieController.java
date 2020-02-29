de90f5fab6982b6979dae848121446de0c6d56af
yunjae <wyj10076@naver.com>
2020-03-01 04:32:40 +0900

    영화 추천기능 구현 및 home.jsp 화면 수정

--- src/main/java/com/main/oneflix/movie/client/controller/MovieController.java
+++ src/main/java/com/main/oneflix/movie/client/controller/MovieController.java
@@ -14,18 +14,22 @@ import com.main.oneflix.genre.service.GetGenreListService;
 import com.main.oneflix.genre.vo.GenreVO;
 import com.main.oneflix.member.vo.MemberVO;
 import com.main.oneflix.movie.service.GetMovieListService;
+import com.main.oneflix.movie.service.GetRecommendMovieListService;
 import com.main.oneflix.movie.vo.MovieVO;
 import com.main.oneflix.review.service.GetReviewListService;
 import com.main.oneflix.review.service.GetReviewService;
 import com.main.oneflix.review.vo.ReviewVO;
 import com.main.oneflix.ticket.service.GetTicketListService;
 import com.main.oneflix.ticket.vo.TicketVO;
+import com.main.oneflix.watch.service.GetCountWatchGenreService;
 
 @Controller
 public class MovieController {
 	
 	@Autowired
 	private GetMovieListService getMovieListService;
+	@Autowired
+	private GetRecommendMovieListService getRecommedMovieListService;
 	@Autowired
 	private GetGenreListService getGenreListService;
 	@Autowired
@@ -57,12 +61,23 @@ public class MovieController {
 	}
 	
 	@RequestMapping("/getMovieListProc.do")
-	public ModelAndView getMovieListProc(MovieVO vo, ModelAndView mav) {
+	public ModelAndView getMovieListProc(MovieVO vo, ModelAndView mav, HttpSession session) {
+		if (vo.getSearchOrder() == null) { vo.setSearchOrder("new"); }
 		vo.setStart(1);
 		vo.setEnd(20);
-		List<MovieVO> movieList = getMovieListService.getMovieList(vo);
+		
 		List<GenreVO> genreList = getGenreListService.getGenreList(new GenreVO());
 		List<TicketVO> ticketList = getTicketListService.getTicketList(new TicketVO());
+		List<MovieVO> movieList;
+		if (vo.getSearchOrder().equals("recommend")) {
+			MemberVO member = (MemberVO)session.getAttribute("member");
+			movieList = getRecommedMovieListService.getRecommendMovieList(vo, member.getEmail(), genreList);
+		} else {
+			movieList = getMovieListService.getMovieList(vo);
+		}
+		
+		mav.addObject("searchGenre", vo.getSearchGenre());
+		mav.addObject("searchOrder", vo.getSearchOrder());
 		mav.addObject("movieType", vo.getMovieType());
 		mav.addObject("movieList", movieList);
 		mav.addObject("genreList", genreList);
