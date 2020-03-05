795c46ba858d3dacea16a985f78ac4d60972a2e7
hyuk <merci726@yahoo.com>
2020-03-06 08:21:28 +0900

    haha

--- src/main/java/com/main/oneflix/alarm/admin/controller/AlarmController.java
+++ src/main/java/com/main/oneflix/alarm/admin/controller/AlarmController.java
@@ -1,14 +1,11 @@
 package com.main.oneflix.alarm.admin.controller;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,20 +16,21 @@ import com.main.oneflix.alarm.service.InsertAlarmService;
 import com.main.oneflix.alarm.service.UpdateAlarmService;
 import com.main.oneflix.movie.service.GetMovieListService;
 import com.main.oneflix.movie.vo.MovieVO;
-
+@Controller	
 public class AlarmController {
+	@Autowired
 	@Resource(name = "insertAlarmService")
 	private InsertAlarmService insertAlarmService;
-	
+	@Autowired
 	@Resource(name = "updateAlarmService")
 	private UpdateAlarmService updateAlarmService;
-	
+	@Autowired
 	@Resource(name = "deleteAlarmService")
 	private DeleteAlarmService deleteAlarmService;
-	
+	@Autowired
 	@Resource(name = "getAlarmListService")
 	private GetAlarmListService getAlarmListService;
-	
+	@Autowired
 	@Resource(name = "getAlarmService")
 	private GetAlarmService getAlarmService;
 	
@@ -42,15 +40,10 @@ public class AlarmController {
 	
 	@RequestMapping("/insertAlarm.do")
 	public ModelAndView insertAlarm(MovieVO vo,ModelAndView mav) {
+		vo.setMovieType("alarm");
+		List<MovieVO> movieList = getMovieListService.getMovieList(vo);
+		mav.addObject("movieList", movieList);
 		mav.setViewName("insertAlarm");
-		List<MovieVO> movieList = getMovieListService.getMovieList(new MovieVO());
-		mav.addObject("movieList",vo);
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(new Date());
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-		System.out.println("현재 날짜 : " + df.format(cal.getTime()));
-		cal.add(Calendar.DATE, +1);
-		System.out.println("전 날짜 : " + df.format(cal.getTime()));
 		return mav;
 	}
 	
--- src/main/java/com/main/oneflix/alarm/service/InsertAlarmService.java
+++ src/main/java/com/main/oneflix/alarm/service/InsertAlarmService.java
@@ -1,10 +1,7 @@
 package com.main.oneflix.alarm.service;
 
-import javax.servlet.http.HttpServletResponse;
-
 import com.main.oneflix.alarm.vo.AlarmVO;
 
 public interface InsertAlarmService {
-	public void selectAlarmList(HttpServletResponse response) throws Exception;
 	public void insertAlarm(AlarmVO vo);
 }
