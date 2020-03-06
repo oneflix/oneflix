b239658e92ed35215a015648fd5bf97678af5028
purple <purple29b@gmail.com>
2020-03-06 13:28:00 +0900

    분석 컨트롤러 조금 만짐

--- src/main/java/com/main/oneflix/analysis/admin/controller/AnalysisController.java
+++ src/main/java/com/main/oneflix/analysis/admin/controller/AnalysisController.java
@@ -1,9 +1,17 @@
 package com.main.oneflix.analysis.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AnalysisController {
+	
+	@RequestMapping("/getAnalysisListProc.mdo")
+	public ModelAndView getAnalysisListProc(ModelAndView mav) {
+		mav.setViewName("analysis");
+		return mav;
+	}
 	
 	
 
