3eac41b64999d1e595e2ff4934fa1d74f7906994
purple29 <58365509+purple29@users.noreply.github.com>
2020-03-09 05:37:30 +0900

    Purple - salse analysis 월간데이터 작업중 (#159)
    * kakao 로그인 코드 정리
    
    * analysis 기능 구현중
    
    * analysis 차트 작업중
    
    * review-mapping.xml 에러 수정
    
    * analysis.jsp 수정
    
    * analysis 작업중
    
    * sales analysis 작업중
    
    * sales analysis 작업중
    
    * sales analysis 작업중
    
    * sales analysis DAO, Service 완성, jsp에 데이터 가져오기
    
    * sales analysis 월간 작업중

--- src/main/java/com/main/oneflix/analysis/service/impl/AnalysisSalesServiceImpl.java
+++ src/main/java/com/main/oneflix/analysis/service/impl/AnalysisSalesServiceImpl.java
@@ -24,7 +24,7 @@ public class AnalysisSalesServiceImpl implements AnalysisSalesService{
 		String salesButton = (String) map.get("salesButton");
 		@SuppressWarnings("unchecked")
 		List<String> yearList = (List<String>) map.get("yearList");
-		Map<String, List<Map<String, Integer>>> monthSalesMap = new HashMap<String, List<Map<String,Integer>>>();;
+		//Map<String, List<Map<String, Integer>>> monthSalesMap = new HashMap<String, List<Map<String,Integer>>>();;
 		SalesVO sales = new SalesVO();
 		if(salesButton.equals("year")) {
 			for(String year : yearList) {
@@ -34,15 +34,34 @@ public class AnalysisSalesServiceImpl implements AnalysisSalesService{
 				response.put(year, analysisDAO.analysisSalesYear(sales));
 			}
 		}else {
+			
 			for(String year : yearList) {
-				sales.setStartDate(year + "0101");
-				String yearPlusOne = Integer.toString(Integer.parseInt(year) + 1);
-				sales.setEndDate(yearPlusOne+"0101");
-				monthSalesMap.put(year, analysisDAO.analysisSalesMonth(sales));
-				System.out.println("service에서 monthSalesMap : " + monthSalesMap);
-			}
+	            sales.setStartDate(year + "0101");
+	            String yearPlusOne = Integer.toString(Integer.parseInt(year) + 1);
+	            sales.setEndDate(yearPlusOne+"0101");
+	            List<Map<String, Object>> tmpList = new ArrayList<>();
+	            for (int i = 1; i <= 12; i++) {
+	               Map<String, Object> tmpMap = new HashMap<>();
+	               tmpMap.put("totalSales", 0);
+	               tmpMap.put("paymentDate", String.valueOf(i));
+	               tmpList.add(i-1, tmpMap);
+	            }
+	            
+	            List<Map<String, Object>> monthList = analysisDAO.analysisSalesMonth(sales);
+	            for (int i = 0; i < monthList.size(); i++) {
+	               for (int j = 0; j < tmpList.size(); j++) {
+	                  if (monthList.get(i).get("paymentDate").equals(
+	                        tmpList.get(j).get("paymentDate"))) {
+	                     tmpList.remove(j);
+	                     tmpList.add(j, monthList.get(i));
+	                     break;
+	                  }
+	               }
+	            }
+	            
+	            response.put(year, tmpList);
+	         }
 		}
-		
 		
 		return response;
 	}
