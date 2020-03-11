c14ad7f0ada87c06b82a18e2547d98ee613964be
jaenyes <60684629+jaenyes@users.noreply.github.com>
2020-03-11 15:04:30 +0900

    Jaen 차트 레이아웃, 서비스 진행중 (#180)
    * 내문의, 문의하기, 상세보기 CSS 변경
    
    * mypageHome, updateMember CSS 정리
    
    * mypageHome, updateMember CSS 정리
    
    * 본인인증 모달,마이페이지홈 구현중
    
    * mypageHome 리뷰슬라이더포함 CSS완성(중요)
    
    * 구글차트 데이터연동완료
    
    * mypageHome 완성
    
    * 마이페이지홈 수정
    
    * login.jsp/login.css 변경, 회원가입페이지 변경중
    
    * login,join,connectSNS jsp css 수정 **중요
    
    * login,join,connectSNS jsp css 수정 후 충돌수정완료
    
    * 로그인페이지 footer 수정
    
    * 로그인, connectSNS keyup이벤트 추가
    
    * 로그인, 회원가입, 소셜로그인 유효성검사 이벤트 완료
    
    * 회원가입 중복검사 완료
    
    * 로그인페이지 중복검사완료
    
    * 본인인증, 탈퇴메일전송후 처리
    
    * 에러페이지 추가(텍스트만 바꿔서 사용)
    
    * google 로그인 삭제 관련 멤버 수정, 영화분석 시작
    
    * 분석진행시작
    
    * 조회수랭킹, 장르별랭킹, 컨텐츠장르분포-> 쿼리작성, 서비스틀
    
    * 서비스정리, 로그인화면 수정
    
    * 영화 분석 진행중
    
    * 차트 틀

--- src/main/java/com/main/oneflix/analysis/service/impl/AnalysisMovieRankServieImpl.java
+++ src/main/java/com/main/oneflix/analysis/service/impl/AnalysisMovieRankServieImpl.java
@@ -1,6 +1,8 @@
 package com.main.oneflix.analysis.service.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.main.oneflix.analysis.dao.AnalysisDAO;
 import com.main.oneflix.analysis.service.AnalysisMovieRankService;
+import com.main.oneflix.movie.vo.MovieVO;
 
 @Service
 public class AnalysisMovieRankServieImpl implements AnalysisMovieRankService {
@@ -18,7 +21,38 @@ public class AnalysisMovieRankServieImpl implements AnalysisMovieRankService {
 	@Override
 	public Map<String, Object> analysisMovieRank(Map<String, Object> map) {
 		Map<String, Object> response = new HashMap<>();
-		// TODO Auto-generated method stub
+//		String movieRankSelect = (String) map.get("movieRankSelect");
+//		@SuppressWarnings("unchecked")
+//		List<String> yearList = (List<String>) map.get("yearList");
+//		MovieVO vo = new MovieVO();
+//		Map<String,String> dbMap = new HashMap<String, String>();
+//		if(movieRankSelect.equals("year")) {
+//			for(String year : yearList) {
+//				dbMap.put("startDate", (year + "0101"));
+//				String yearPlusOne = Integer.toString(Integer.parseInt(year)+1);
+//				dbMap.put("endDate", (yearPlusOne + "0101"));
+//				response.put(year, analysisDAO.analysisMovieRankYear(dbMap));
+//			}
+//		} else {
+//			
+//			for(String year : yearList) {
+//				dbMap.put("startDate", (year + "0101"));
+//				String yearPlusOne = Integer.toString(Integer.parseInt(year));
+//				dbMap.put("endDate", (yearPlusOne + "0101"));
+//				List<List<Map<String, Object>>> tmpList = new ArrayList<>();
+//				for(int i = 1; i <= 12; i++) {
+//					Map<String, Object> tmpMap = new HashMap<>();
+//					tmpMap.put("count", 0);
+//					tmpMap.put("movieTitle", String.valueOf(i));
+//					tmpMap.put("watchDate", String.valueOf(i));
+//					tmpList.add(i-1, tmpMap);
+//				}
+//				
+//				
+//			}
+//				
+//		}
+		
 		
 		return response;
 	}
