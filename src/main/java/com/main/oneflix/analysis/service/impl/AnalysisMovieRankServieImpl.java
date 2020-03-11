33a200133297cb3129c1dcba7666d4ad30737831
jaenyes <jaenyes17@gmail.com>
2020-03-11 09:30:50 +0900

    영화 분석 진행중

--- src/main/java/com/main/oneflix/analysis/service/impl/AnalysisMovieRankServieImpl.java
+++ src/main/java/com/main/oneflix/analysis/service/impl/AnalysisMovieRankServieImpl.java
@@ -1,6 +1,7 @@
 package com.main.oneflix.analysis.service.impl;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +19,20 @@ public class AnalysisMovieRankServieImpl implements AnalysisMovieRankService {
 	@Override
 	public Map<String, Object> analysisMovieRank(Map<String, Object> map) {
 		Map<String, Object> response = new HashMap<>();
-		// TODO Auto-generated method stub
+		String movieRankSelect = (String) map.get("movieRankSelect");
+		@SuppressWarnings("unchecked")
+		List<String> yearList = (List<String>) map.get("yearList");
+		
+		Map<String,String> dbMap = new HashMap<String, String>();
+		if(movieRankSelect.equals("year")) {
+			for(String year : yearList) {
+				dbMap.put("startDate", (year + "0101"));
+				String yearPlusOne = Integer.toString(Integer.parseInt(year)+1);
+				dbMap.put("endDate", (yearPlusOne + "0101"));
+				response.put(key, value)
+			}
+		}
+		
 		
 		return response;
 	}
