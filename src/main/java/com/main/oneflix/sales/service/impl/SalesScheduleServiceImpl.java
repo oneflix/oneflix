e569010002a61828b8314ccc9502e612f7e533a7
jaenyes <60684629+jaenyes@users.noreply.github.com>
2020-03-16 14:20:10 +0900

    Revert "Purple - Sales, KakaoPay, ReviewLike Transaction 기능 추가 (#214)" (#218)
    This reverts commit fa16f5e439b9129de2efd7f35a81e962f65c1ebd.

--- src/main/java/com/main/oneflix/sales/service/impl/SalesScheduleServiceImpl.java
+++ src/main/java/com/main/oneflix/sales/service/impl/SalesScheduleServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -23,7 +22,6 @@ import com.main.oneflix.sales.vo.SalesVO;
 import com.main.oneflix.util.factory.rest.RestTemplateFactory;
 import com.main.oneflix.util.kakao.KakaoConst;
 
-@Transactional
 @Service
 public class SalesScheduleServiceImpl implements KakaoConst, SalesScheduleService {
 
