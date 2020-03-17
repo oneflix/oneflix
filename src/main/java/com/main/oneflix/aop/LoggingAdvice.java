e286932f7283fe42f87fe07b4d3bdb3fe8b0b4a9
yunjae <wyj10076@naver.com>
2020-03-17 18:52:05 +0900

    로그 메세지 수정

--- src/main/java/com/main/oneflix/aop/LoggingAdvice.java
+++ src/main/java/com/main/oneflix/aop/LoggingAdvice.java
@@ -3,11 +3,19 @@ package com.main.oneflix.aop;
 
 import java.text.DecimalFormat;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.main.oneflix.manager.vo.ManagerVO;
+import com.main.oneflix.member.vo.MemberVO;
 
 @Aspect
 public class LoggingAdvice {
@@ -15,6 +23,19 @@ public class LoggingAdvice {
 	@Around("execution(* com.main.oneflix..*ServiceImpl.*(..))") 
 	public Object logPrint(ProceedingJoinPoint joinPoint) throws Throwable {
 
+		HttpServletRequest request = 
+		        ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+		String ip = request.getRemoteAddr();
+		HttpSession session = request.getSession();
+		String id = "";
+		if (session.getAttribute("member") != null) {
+			MemberVO member = (MemberVO)session.getAttribute("member");
+			id = member.getEmail();
+		} else if (session.getAttribute("loginManager") != null) {
+			ManagerVO manager = (ManagerVO)session.getAttribute("loginManager");
+			id = manager.getManagerId();
+		}
+		
 		long start = System.currentTimeMillis();
 		try {
 			return joinPoint.proceed();   
@@ -22,7 +43,7 @@ public class LoggingAdvice {
 			DecimalFormat formatter = new DecimalFormat("###,###");
 			long finish = System.currentTimeMillis();
 			
-			LOGGER.info(joinPoint.getSignature().toShortString() + 
+			LOGGER.info("[" + ip + "/" + id + "]\n\t" + joinPoint.getSignature().toShortString() + 
 					" in [" + formatter.format(finish - start) + "] ms");
 			
 		}
